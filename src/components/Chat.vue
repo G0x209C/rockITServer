d0da618cb8fc822592788b86c5db94b11ae9a92c
G0x209C <g@collette-online.nl>
2021-11-03 14:25:09 +0100

    :smile: Initial ChatComponent: Chat.vue

---
+++ src/components/Chat.vue
@@ -0,0 +1,40 @@
+<template>
+  <!-- TODO: ADD V-if for if chat needs to be shown -->
+  <div>
+    <textarea name="chatbox" id="chatbox" cols="30" rows="10">
+    </textarea>
+    <input type="text"><input type="submit" id="chat.submit" value="Send">
+  </div>
+</template>
+
+<script>
+export default
+{
+  data()
+  {
+    return {
+      isConnected: false,
+      socketMessage: ''
+    }
+  },
+  sockets:
+      {
+        connect()
+        {
+          this.isConnected = true;
+        },
+        disconnect()
+        {
+          this.isConnected=false;
+        }
+      },
+  methods:
+      {
+        sendChat(data)
+        {
+          this.$socket.emit('chat.send', data)
+        }
+      }
+}
+</script>
+
