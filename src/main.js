3ba0591796a536dc9facd249956e761fd643d033
G0x209C <g@collette-online.nl>
2021-11-03 14:26:28 +0100

    :smile: Added Socket.io-client and vue-socket.io-extended: socket-connection

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
+import { io } from 'socket.io-client';
+import VueSocketIOExt from "vue-socket.io-extended";
 
-createApp(App).use(router).mount('#app')
+const socket = io('http://localhost:3000');
+
+createApp(App).use(store).use(VueSocketIOExt, socket)
+    .use(router).mount('#app');
