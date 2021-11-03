464e6ceb4ce81825f4556dfb11cb8232937e4a43
G0x209C <g@collette-online.nl>
2021-11-03 14:08:16 +0100

    :godmode: client.js Added fields and functions
    Client object now also holds score, name and is_host.
    Added data return function so that Socket.IO can reflect Client-state information to the connected client.

--- app/client.js
+++ app/client.js
@@ -3,25 +3,40 @@
 class Client
 {
     // TODO: look into using UUID to relink client to socket.
-    uuid;
+    uuid = "UIXIDAS";
+    name = "Player";
     socket; // TODO: implement: on disconnect, needs to be reassigned to new socket;
-    room; // this will come in very handy, trust me :)
-    player;
+    is_host = false; // set permissions: will be done on init TODO: implement this.
+    room = "AAAAA"; // this will come in very handy, trust me :)
+    score = 0;
 
-    setRoom(room)
-    {
-
-    }
-
-    constructor(socket) {
+    constructor(io, socket) {
         //  TODO: generate uuid and send to client;
         //
         this.socket=socket;
     }
 
+    setRoom(room)
+    {
+        this.room = room;
+        // TODO: add aditional checks before adding user to room
+        this.socket.join(room);
+    }
+
+
     sendMessage(msg)
     {
 
+    }
+
+    returnDatamodel()
+    {
+        return {
+            uuid: this.uuid,
+            is_host: this.is_host,
+            room: this.room,
+            score: this.score
+        }
     }
 }
 
