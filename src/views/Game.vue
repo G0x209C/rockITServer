37bc26db5f04fcabb0ee90cde54c31d0723be433
G0x209C <g@collette-online.nl>
2021-11-03 14:25:28 +0100

    :smile: Initial GameComponent and View

---
+++ src/views/Game.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <!-- TODO: Add dynamic game component -->
+    <Game></Game>
+    <chat> <!-- Added chat component to screen -->
+    </chat>
+  </div>
+</template>
+
+<script>
+import Chat from "../components/Chat";
+import Game from "../components/Game";
+
+export default
+{
+  props:[
+      'player'
+  ],
+  sockets:{
+
+  },
+  components:
+      {
+        Chat,
+        Game
+      }
+}
+</script>
+
