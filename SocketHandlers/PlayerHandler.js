c6d2600a00be776a32e170db0ee4f08845d767db
G0x209C <g@collette-online.nl>
2021-11-03 14:09:37 +0100

    :sweat_smile: Added documentation to PlayerHandler.js

--- SocketHandlers/PlayerHandler.js
+++ SocketHandlers/PlayerHandler.js
@@ -1,4 +1,12 @@
-
+/**
+ * @author G0x209C
+ * @params io, client
+ * @description Player handler that handles player operations
+ *
+ * This component acts like a Controller:
+ * It is added to the socket connection and handles the correct function calls
+ * needed for the socket's event.
+ */
 module.exports = (io,socket)=>
 {
     socket.on('yay',(data)=>{
