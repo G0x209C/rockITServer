cfd72b45db2d81bac2e89d65f6c2cf52a322e486
G0x209C <g@collette-online.nl>
2021-12-21 12:51:42 +0100

    :godmode: Expanded on newGame functionality
    newGame now supports 2 separate operations depending on input.
    If given a roomcode, it will try to join that room.
    If not given one, it will attempt to create a new room and join the player to it.

--- api/controllers/game/newgame.js
+++ api/controllers/game/newgame.js
@@ -1,5 +1,4 @@
 
-
 module.exports = {
 
 
@@ -10,30 +9,62 @@ module.exports = {
 
 
   inputs: {
-    name:{type:'string',required:true}
+    name:{type:'string', required:true},
+    room:{type:'string', required:false}
   },
 
 
   exits: {
-
+    success:{
+      responseType: ''
+    }
   },
 
 
   fn: async function (inputs) {
 
-    // generate a room code
-    let code = await sails.helpers.genCode(); // get initial code
-    while(await Room.count({roomId:code})>0){ // check if DB already has a record: if so, get new code.
-      code = await sails.helpers.genCode();
+    let users;
+
+    if(inputs.room){
+      if(await Room.count({roomId:inputs.room})>0){
+        let room = await Room.findOne({roomId:inputs.room});
+
+        // create player joined to room.
+
+        let player = await Player.create(
+          {
+            name:inputs.name,
+            room: await room.id
+          });
+      }
+      else
+      {
+        return 'Room not found';
+      }
+
+      users = await Room.find({roomId:inputs.room}).populate('players');
+    }else
+    {
+      // generate a room code
+      let code = await sails.helpers.genCode(); // get initial code
+      while(await Room.count({roomId:code})>0){ // check if DB already has a record: if so, get new code.
+        code = await sails.helpers.genCode();
+      }
+      let room = await Room.create({
+        roomId: code,
+      }).fetch();
+      // create new player in database
+      let player = await Player.create({
+        name:inputs.name,
+        room: await room.id
+      }).fetch();
+      users = await Room.find({roomId:code}).populate('players');
     }
-    //await Room.create();
-    // create new player in database
-    await Player.create({
-      name:'henk',
-      room:code
-    });
+
+
+
     // All done.
-    return code;
+    return users;
 
   }
 
