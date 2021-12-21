3761935ec45900e062c36f17e551a34d63ecd4b3
G0x209C <g@collette-online.nl>
2021-12-21 12:50:04 +0100

    :package: Removed obsolete field from Room.js model

--- api/models/Room.js
+++ api/models/Room.js
@@ -5,7 +5,6 @@ module.exports = {
   attributes:
     {
       roomId: {type:'string', required:true},
-      userCount: {type:'number', defaultsTo:0},
 
       players:{
         collection:'player',
