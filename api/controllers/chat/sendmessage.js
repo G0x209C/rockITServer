908b39a531b30a2f2956c2a4e9a5d25984be7285
G0x209C <g@collette-online.nl>
2021-12-22 17:11:16 +0100

    :package: New action: sendChat

---
+++ api/controllers/chat/sendmessage.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+
+  friendlyName: 'Sendmessage',
+
+
+  description: 'Sendmessage chat.',
+
+
+  inputs: {
+      msg:{type:'string', required:true}
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs) {
+
+    // All done.
+    return;
+
+  }
+
+
+};
