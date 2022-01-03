fdc9216a1b9cd50c4183c15c882b764bac291f19
G0x209C <g@collette-online.nl>
2022-01-03 21:55:47 +0100

    :smile: Improved the socket-side

--- api/controllers/chat/sockets/subscribetoroom.js
+++ api/controllers/chat/sockets/subscribetoroom.js
@@ -19,17 +19,17 @@ module.exports = {
 
   fn: async function (inputs, exits, env) {
 
-    // if(!this.req.isSocket){
-    //   throw {badRequest:'Only sockets can subscribe to a room. The request was probably HTTP-only'};
-    // } TODO: enable this when client is finished
+    if(!this.req.isSocket){
+      throw {badRequest:'Only sockets can subscribe to a room. The request was probably HTTP-only'};
+    }
     let player = await Player.findOne({secret:inputs.secret}).populate('room'); // get room associated to user
     //let messages = await Message.find({room:"henk"});
 
     //let connRoom = await Room.subscribe(env.req, _.pluck(room,'id')); // subscribe user to Room;
 
     // this should subscribe the user to the right room.
-    sails.sockets.addRoomMembersToRooms(Player.getRoomName(player.id), Room.getRoomName(player.room.id));
-    return env.res.ok();
+    sails.sockets.join(env.req, player.room.roomId);
+    return env.res.ok(player);
 
   }
 
