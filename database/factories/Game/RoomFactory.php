a4c93dfcfcfb2660dfdb51f3108a0a45225c1a80
G0x209C <g@collette-online.nl>
2022-01-05 02:11:42 +0100

    :package: First commit

---
+++ database/factories/Game/RoomFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories\Game;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class RoomFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
